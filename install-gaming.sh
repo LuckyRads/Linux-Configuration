5eb4570e04ff822932fe4222fc78b04f0c5d0b1f
LuckyRads <50592620+LuckyRads@users.noreply.github.com>
2021-03-22 01:43:30 +0200

    ADD: Gaming peripheral installation script.

---
+++ install-gaming.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo This script installs gaming peripheral software:
+echo Piper (for gaming mice)
+echo LiquidCtl (for NZXT controllers)
+
+sudo apt install piper
+
+sudo apt install python3
+sudo apt install python3-pip
+pip3 install liquidctl
