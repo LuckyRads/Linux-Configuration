3348874fb844dc9a7b4bceb557b8443d92483f34
Matas M <50592620+LuckyRads@users.noreply.github.com>
2021-10-18 01:09:32 +0300

    Update install-gaming.sh

--- install-gaming.sh
+++ install-gaming.sh
@@ -9,3 +9,15 @@ sudo apt install piper
 sudo apt install python3
 sudo apt install python3-pip
 pip3 install liquidctl
+
+echo To configure LiquidCtl please see the documentation. /etc/systemd/system/liquidcfg.service contains the boot configuration.
+echo Sample file for NZXT LEDs:
+echo [Unit]
+echo Description=NZXT startup service
+echo 
+echo [Service]
+echo Type=oneshot
+echo ExecStart=liquidctl set led1 color fixed bb00ff
+echo 
+echo [Install]
+echo WantedBy=default.target
