b78604a13bcb6ba8e49ca0dc9c843e14c533f6f9
Matas M <50592620+LuckyRads@users.noreply.github.com>
2021-10-25 20:10:34 +0300

    Update install-core.sh

--- install-core.sh
+++ install-core.sh
@@ -14,13 +14,7 @@ echo Legion5 specific: GRUB_CMDLINE_LINUX_DEFAULT="queit splash
  acpi_backlight=intel acpi_osi=linux"
 echo Legion5 specific: change to Dedicated graphics in bios in order for the brightness control to work
 echo Terminator terminal
-echo New profile
-echo Colors: use colors from system theme
-echo Transparent background set to 0.90
-echo Layout set terminal to created custom profile
-echo Add keybindings to switch to tabs (alt + numbers)
-echo Copy from functions comment to the EOF https://raw.githubusercontent.com/ChrisTitusTech/scripts/master/fancy-bash-promt.sh and paste it in .bashrc file
-
+echo Paste Terminator configuration file terminator_config to/as ~/.config/terminator/config
 
 sudo apt update
 
