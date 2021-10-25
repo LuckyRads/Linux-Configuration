402f1f41c45250e5356a66b7eec3e021b4696497
Matas M <50592620+LuckyRads@users.noreply.github.com>
2021-10-25 17:06:15 +0300

    Update install-core.sh

--- install-core.sh
+++ install-core.sh
@@ -4,6 +4,15 @@ echo Installation content:
 echo Gnome tweak tools
 echo Hardware information
 echo Dconf editor
+echo Configure GRUB dual boot
+echo /etc/default/grub
+echo GRUB_DEFAULT=saved
+echo GRUB_SAVEDEFAULT=true
+echo GRUB_TIMEOUT_STYLE=hidden
+echo GRUB_TIMEOUT=10
+echo Legion5 specific: GRUB_CMDLINE_LINUX_DEFAULT="queit splash
+ acpi_backlight=intel acpi_osi=linux"
+echo Legion5 specific: change to Dedicated graphics in bios in order for the brightness control to work
 
 sudo apt update
 
