3822608d2ebb796d931338a76e14754a8c946791
Matas M <50592620+LuckyRads@users.noreply.github.com>
2021-10-25 16:56:04 +0300

    Update install-gui.sh

--- install-gui.sh
+++ install-gui.sh
@@ -9,7 +9,8 @@ echo Install Sound input \& output device chooser extension
 echo Configure Ubuntu dock with dconf editor
 echo Configure Arcmenu extension with gnome-tweak-tools
 echo gTile Gnome extension for window management
-echo gTile add 2x2 4x4 grids
+echo gTile add 2x2 grid to the end of the line
+echo gTile transfer 4x4 grid to the front of the line
 echo gTile set window margin to 5
 echo
 echo Font settings
