ab7a55ca4ad70a875caac7e2eeadddb880bc4780
Matas M <50592620+LuckyRads@users.noreply.github.com>
2021-10-26 16:08:19 +0300

    Update install-core.sh

--- install-core.sh
+++ install-core.sh
@@ -4,15 +4,18 @@ echo Installation content:
 echo Gnome tweak tools
 echo Hardware information
 echo Dconf editor
+
 echo Configure GRUB dual boot
 echo /etc/default/grub
 echo GRUB_DEFAULT=saved
 echo GRUB_SAVEDEFAULT=true
 echo GRUB_TIMEOUT_STYLE=menu
 echo GRUB_TIMEOUT=10
+
 echo Legion5 specific: GRUB_CMDLINE_LINUX_DEFAULT="queit splash
  acpi_backlight=intel acpi_osi=linux"
 echo Legion5 specific: change to Dedicated graphics in bios in order for the brightness control to work
+
 echo Terminator terminal
 echo Paste Terminator configuration file terminator_config to/as ~/.config/terminator/config
 echo Install fonts-powerline
