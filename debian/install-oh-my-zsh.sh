28a0baebf4345f83db3d572a016e7d8f56a5b3c9
Matas <luckyrads@gmail.com>
2021-12-14 00:17:30 +0200

    ADD: ZSH autocompletion plugin.

--- debian/install-oh-my-zsh.sh
+++ debian/install-oh-my-zsh.sh
@@ -13,6 +13,9 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 # powerlevel10k theme installation
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# autocompletion plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 echo oh-my-zsh and powerline10k theme should be installed.
 echo Restart the terminal to see the changes.
 echo In order to use the powerline10k theme:
